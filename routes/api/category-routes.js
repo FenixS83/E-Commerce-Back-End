45d6c0b500fa33437bf038972a5cbee22a3224e1
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-28 18:13:51 -0400

    added the -delete a category by its id- section to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -72,8 +72,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        category_id: req.body.category_id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({
+        message: 'No category found with that id!'
+      });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
