6b162795dfa50aa0d6f6bbaf00cbc184f788f107
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-29 17:06:35 -0400

    added -find single tag by id- section to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,8 +20,23 @@ router.get('/', async (req, res) => {
 
 // find a single tag by its `id`
 // be sure to include its associated Product data
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: {
+        model: Product
+      },
+    });
+    if (!tagData) {
+      res.status(404).json ({
+        message: 'No tag found with that ID'
+      });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create a new tag
